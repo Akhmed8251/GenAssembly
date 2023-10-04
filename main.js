28490dfd0276b3f5ff8f20de9812b83510be5c5c
Akhmed8251 <bratya_250@mail.ru>
2023-10-04 16:40:24 +0300

    верстка desktop версии и функция переключения на другой язык

---
+++ main.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const switchLangElements = document.querySelectorAll(".switch-language__item")
+    switchLangElements.forEach(langEl => {
+        langEl.addEventListener("click", () => {       
+            if (!langEl.classList.contains("active")) {
+                const currentLang = document.querySelector(".switch-language__item.active")
+                currentLang.classList.remove("active")
+                langEl.classList.add("active")
+
+                const langBlockRus = document.querySelector(".main-content--rus")
+                const langBlockEng = document.querySelector(".main-content--eng")
+                const logoImage = document.querySelector(".logo img")
+                if (langEl.dataset.lang == "rus") {
+                    langBlockRus.style.display = "block"
+                    langBlockEng.style.display = "none"
+                    logoImage.src = "/img/logo.svg"
+                    logoImage.alt = "Логотип ДГУ"
+                } else {
+                    langBlockEng.style.display = "block"
+                    langBlockRus.style.display = "none"
+                    logoImage.src = "/img/logo-eng.svg"
+                    logoImage.alt = "DSU logo"
+                }
+            }
+        })
+    })
+
+    const accordeons = document.querySelectorAll(".accordeon__item")
+    accordeons.forEach(accordeonEl => {
+        accordeonEl.addEventListener("click", function(evt) {
+            if (evt.target.closest(".accordeon__control")) {
+                accordeonEl.classList.toggle("active")
+
+                const accordeonContent = accordeonEl.querySelector(".accordeon__content")
+                if (accordeonEl.classList.contains("active")) {
+                    accordeonContent.style.maxHeight = accordeonContent.scrollHeight + 'px';
+                } else {
+                    accordeonContent.style.maxHeight = null;
+                }
+            }
+        })
+    })
+})
